159c8d473475593377478d70359e487a1a6dd100
mavillar <marcos.villar@mercadolibre.com>
2022-10-09 20:46:47 -0300

    feature: initial commit

---
+++ src/components/features/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Features = () => {
+  return (
+    <div className='features'>
+      <h3 className='features__title'>Relajate, hacemos todo por ti:</h3>
+      <div className='features__containerfeatures'>
+        <div className='features__containerfeatures__feature'>
+          <img
+            src='https://cdn-icons-png.flaticon.com/512/4350/4350190.png'
+            alt='feature'
+          />
+          <h4 className='features__containerfeatures__feature__title'>
+            Variedad de propiedades
+          </h4>
+          <p className='features__containerfeatures__feature__description'>
+            Mas de 600 propiedades en toda america
+          </p>
+        </div>
+
+        <div className='features__containerfeatures__feature'>
+          <img
+            src='https://cdn-icons-png.flaticon.com/512/1473/1473559.png'
+            alt='feature'
+          />
+          <h4 className='features__containerfeatures__feature__title'>
+            Variedad de propiedades
+          </h4>
+          <p className='features__containerfeatures__feature__description'>
+            Mas de 600 propiedades en toda america
+          </p>
+        </div>
+        <div className='features__containerfeatures__feature'>
+          <img
+            src='https://cdn-icons-png.flaticon.com/512/3058/3058193.png'
+            alt='feature'
+          />
+          <h4 className='features__containerfeatures__feature__title'>
+            Variedad de propiedades
+          </h4>
+          <p className='features__containerfeatures__feature__description'>
+            Mas de 600 propiedades en toda america
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,20 @@
+import HeadHome from '../../components/head';
+import Features from '../../components/features';
+import AboutUs from '../../components/aboutUs';
+import Footer from '../../components/footer';
+
+const Home = () => {
+  return (
+    <>
+      <HeadHome />
+      <main>
+        <section id='body' >
+          <Features />
+        </section>
+        <AboutUs />
+      </main>
+      <Footer />
+    </>
+  );
+};
+export default Home;
